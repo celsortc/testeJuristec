fbd00240fca78aa89565cbcb881b15ae5461ba68
Celso Toledo <105131736+celsortc@users.noreply.github.com>
2026-03-27 21:35:58 -0300

    file1

---
+++ Questao1/q1.py
@@ -0,0 +1,7 @@
+##### -----Resposta abaixo:
+
+# Provavelmente indica que os dados estão sendo carregados via JS. Utilizaria o DevTools para verificar a existência de uma API. caso exista, pegaria os dados direto da fonte.
+
+# Se não for possível, utilizaria ferramentas que executam o JS, como Selenium.
+
+# Quanto ao erro, provavelmente indica um bloqueio do servidor para as requests devido ao comportamento. Eu limitaria a velocidade ou simularia um navegador.
